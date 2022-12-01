c888056bf65bda9982c210de4e03824783c95646
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-01 23:31:14 +0700

    sobel operator

---
+++ Convolution/ConvolutionCalc.cs
@@ -0,0 +1,19 @@
+﻿using ComputerVisionDemo.Tool;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ComputerVisionDemo.Convolution {
+
+    public class ConvolutionCalc {
+
+        // asser that kernel height and width are equal, and are odds
+        static public double Calc(Image<Gray, byte> src, double[,] kernel, int y, int x) {
+            double res = 0;
+            int k = kernel.GetLength(0)>>1;
+            for (int i = 0; i < kernel.GetLength(0); i++)
+                for (int j = 0; j < kernel.GetLength(1); j++)
+                    res += kernel[i, j] * src[y + i - k, x + j - k].Intensity;
+            return res;
+        }
+    }
+}
---
+++ Convolution/SobelOperator.cs
@@ -0,0 +1,35 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using ComputerVisionDemo.Tool;
+
+namespace ComputerVisionDemo.Convolution {
+
+    public class SobelOperator {
+
+        public static Image<Gray, byte> Apply(Image<Gray, byte> src, double[,] kernel) {
+            Image<Gray, byte> res = new(src.Size);
+            int k = kernel.GetLength(0) >> 1;
+            ImgTool<Gray, byte>.Forward(src, k, k, src.Height - k, src.Width - k, (y, x) => {
+                res[y, x] = new Gray(ConvolutionCalc.Calc(src, kernel, y, x));
+                return true;
+            });
+            return res;
+        }
+
+        // negative cells are displayed as red
+        // while positive cells are displayed as blue
+        public static Image<Bgr, byte> VisualApply(Image<Gray, byte> src, double[,] kernel) {
+            Image<Bgr, byte> res = new(src.Size);
+            int k = kernel.GetLength(0) >> 1;
+            ImgTool<Gray, byte>.Forward(src, k, k, src.Height - k, src.Width - k, (y, x) => {
+                double w = ConvolutionCalc.Calc(src, kernel, y, x);
+                if (w < 0)
+                    res[y, x] = new Bgr(0, 0, -w);
+                else
+                    res[y, x] = new Bgr(w, 0, 0);
+                return true;
+            });
+            return res;
+        }
+    }
+}
