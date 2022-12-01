fe8cafa354ab08001a47cbcccf9e25f085c15724
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-01 23:31:01 +0700

    tools from FingerprintRecognition

---
+++ Tool/ImgTool.cs
@@ -0,0 +1,22 @@
+﻿using Emgu.CV;
+
+namespace ComputerVisionDemo.Tool {
+
+    public class ImgTool<TColor, TDepth> where TColor : struct, IColor where TDepth : new() {
+
+        /** @ the core */
+        static public void Forward(Image<TColor, TDepth> src, Func<int, int, bool> f) {
+            Forward(src, 0, 0, src.Height, src.Width, f);
+        }
+
+        static public void Forward(Image<TColor, TDepth> src, int t, int l, int d, int r, Func<int, int, bool> f) {
+            t = Math.Max(0, t);
+            l = Math.Max(0, l);
+            d = Math.Min(src.Height, d);
+            r = Math.Min(src.Width, r);
+            for (int y = t; y < d; y++)
+                for (int x = l; x < r; x++)
+                    f(y, x);
+        }
+    }
+}
---
+++ Tool/Iterator2D.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ComputerVisionDemo.Tool {
+
+    public class Iterator2D {
+
+        /** @ the core */
+        static public void Forward(int h, int w, Func<int, int, bool> f) {
+            Forward(0, 0, h, w, f);
+        }
+
+        static public void Forward(int t, int l, int d, int r, Func<int, int, bool> f) {
+            for (int y = t; y < d; y++)
+                for (int x = l; x < r; x++)
+                    f(y, x);
+        }
+    }
+}
