f288b7c3034f315afe072bac80c75e551555dd9f
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-12-07 15:06:46 +0700

    sample image

--- Program.cs
+++ Program.cs
@@ -6,5 +6,5 @@ using Emgu.CV.Structure;
 const string IMG_DIR = "D:\\r\\repos\\ComputerVisionDemo\\sample-images\\";
 
 Image<Gray, byte> src = new(IMG_DIR + "sobel-src-2.jpg");
-Image<Gray, byte> des = SobelOperator.Apply(src, SobelOperator.X_KERNEL);
-CvInvoke.Imwrite(IMG_DIR + "sobel-edge-2.png", des);
+Image<Bgr, byte> des = SobelOperator.VisualApply(src, SobelOperator.X_KERNEL);
+CvInvoke.Imwrite(IMG_DIR + "sobel-visual-2.png", des);
